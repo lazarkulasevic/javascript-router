bda91f3f89400e353bf3c0a6a9b1ae5411932ea3
lazar.kulasevic <lazar.kulasevic@gmail.com>
2021-06-15 18:24:11 +0200

    go build again

--- client/static/js/api/index.js
+++ client/static/js/api/index.js
@@ -1,4 +1,4 @@
-// const API = 'https://javascript-router.herokuapp.com/database'
-const API = 'http://localhost:5500/database' // dev mode
+const API = 'https://javascript-router.herokuapp.com/database'
+// const API = 'http://localhost:5500/database' // dev mode
 
 export default API
